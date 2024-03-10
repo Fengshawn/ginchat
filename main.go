ad5fdeadbd05ac4c377df68fbecbbfc325567501
Fengshawn <511097569@qq.com>
2024-03-10 23:48:31 +0800

    change main entry

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginchat/router"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r := router.Router()
+	r.Run(":8081") // listen and serve on 0.0.0.0:8080
 }
