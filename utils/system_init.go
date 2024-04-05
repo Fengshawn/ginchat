821188b0f9db983259609dc83e0ab271e36c8933
Fengshawn <511097569@qq.com>
2024-04-05 18:42:39 +0800

    add Redis

--- utils/system_init.go
+++ utils/system_init.go
@@ -6,13 +6,17 @@ import (
 	"os"
 	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-var DB *gorm.DB
+var (
+	DB  *gorm.DB
+	Red *redis.Client
+)
 
 func InitConfig() {
 	viper.SetConfigName("app")
@@ -37,4 +41,22 @@ func InitMySQL() {
 	DB, _ = gorm.Open(mysql.Open(viper.GetString("mysql.dns")),
 		&gorm.Config{Logger: newLogger})
 	fmt.Println("Mysql inited...")
+}
+
+func InitRedis() {
+	Red = redis.NewClient(&redis.Options{
+		Addr:         viper.GetString("redis.addr"),
+		Password:     viper.GetString("redis.PASSWORD"),
+		DB:           viper.GetInt("redis.DB"),
+		PoolSize:     viper.GetInt("redis.poolSize"),
+		MinIdleConns: viper.GetInt("redis.minIdleConns"),
+	})
+	pong, err := Red.Ping().Result()
+	if err != nil {
+		fmt.Println("init Redis error ...", err)
+	} else {
+		fmt.Println("init Redis ...", pong)
+		fmt.Println("Redis inited...")
+	}
+
 }
