1694d786b19a8616ba2a3d19924099377a659e75
Fengshawn <511097569@qq.com>
2024-04-08 16:20:48 +0800

    add message chat function

--- models/message.go
+++ models/message.go
@@ -1,79 +1,191 @@
 package models
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"strconv"
-// 	"sync"
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+	"sync"
 
-// 	"github.com/gorilla/websocket"
-// 	"gopkg.in/fatih/set.v0"
-// 	"gorm.io/gorm"
-// )
+	"github.com/gorilla/websocket"
+	"gopkg.in/fatih/set.v0"
+	"gorm.io/gorm"
+)
 
-// // 消息
-// type Message struct {
-// 	gorm.Model
-// 	FromId   string //发送者
-// 	TargetId string // 接收者
-// 	Type     string //消息类型  群聊  私聊  广播
-// 	Media    int    //消息类型  文字 图片  音频
-// 	Content  string //消息内容
-// 	Pic      string
-// 	Url      string
-// 	Desc     string
-// 	Amount   int //其他数字统计
-// }
+// 消息
+type Message struct {
+	gorm.Model
+	FromId   int64  //发送者
+	TargetId int64  // 接收者
+	Type     int    //消息类型  群聊  私聊  广播
+	Media    int    //消息类型  文字 图片  音频
+	Content  string //消息内容
+	Pic      string
+	Url      string
+	Desc     string
+	Amount   int //其他数字统计
+}
 
-// func (table *Message) TableName() string {
-// 	return "message"
-// }
+func (table *Message) TableName() string {
+	return "message"
+}
 
-// type Node struct {
-// 	Conn      *websocket.Conn
-// 	DataQueue chan []byte
-// 	GroupSets set.Interface
-// }
+type Node struct {
+	Conn      *websocket.Conn
+	DataQueue chan []byte
+	GroupSets set.Interface
+}
 
-// // 映射关系
-// var clientMap map[int64]*Node = make(map[int64]*Node, 0)
+// 映射关系
+var clientMap map[int64]*Node = make(map[int64]*Node, 0)
 
-// // 读写锁
-// var rwLocker sync.RWMutex
+// 读写锁
+var rwLocker sync.RWMutex
 
-// func Chat(writer http.ResponseWriter, request *http.Request) {
-// 	//1. 检验 token
-// 	query := request.URL.Query()
-// 	Id := query.Get("userId")
-// 	userId, _ := strconv.ParseInt(Id, 10, 64)
-// 	token := query.Get("token")
-// 	targetId := query.Get("targetId")
-// 	context := query.Get("context")
-// 	isvalida := true // checkToken() Todo
-// 	msgType := query.Get("type")
-// 	conn, err := (&websocket.Upgrader{
-// 		CheckOrigin: func(r *http.Request) bool {
-// 			return isvalidate
-// 		},
-// 	}).Upgrade(writer, request, nil)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-// 	//2. 获取conn
-// 	node := &Node{
-// 		Conn:      conn,
-// 		DataQueue: make(chan []byte, 50),
-// 		GroupSets: set.New(set.ThreadSafe),
-// 	}
-// 	//3. 用户关系
-// 	//4. userid跟node绑定
-// 	rwLocker.Lock()
-// 	clientMap[userId] = node
-// 	rwLocker.Unlock()
+func Chat(writer http.ResponseWriter, request *http.Request) {
+	//1. 检验 token
+	query := request.URL.Query()
+	Id := query.Get("userId")
+	userId, _ := strconv.ParseInt(Id, 10, 64)
+	//token := query.Get("token")
+	//targetId := query.Get("targetId")
+	//context := query.Get("context")
+	isvalidate := true // checkToken() Todo
+	//msgType := query.Get("type")
+	conn, err := (&websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return isvalidate
+		},
+	}).Upgrade(writer, request, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//2. 获取conn
+	node := &Node{
+		Conn:      conn,
+		DataQueue: make(chan []byte, 50),
+		GroupSets: set.New(set.ThreadSafe),
+	}
+	//3. 用户关系
+	//4. userid跟node绑定
+	rwLocker.Lock()
+	clientMap[userId] = node
+	rwLocker.Unlock()
 
-// 	//5. 完成发送逻辑
-// 	go sendProc(node)
-// 	//6. 完成接收逻辑
-// 	go recvProc(node)
-// }
+	//5. 完成发送逻辑
+	go sendProc(node)
+	//6. 完成接收逻辑
+	go recvProc(node)
+	sendMsg(userId, []byte("欢迎进入聊天系统"))
+}
+
+func sendProc(node *Node) {
+	for {
+		select {
+		case data := <-node.DataQueue:
+			err := node.Conn.WriteMessage(websocket.TextMessage, data)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func recvProc(node *Node) {
+	for {
+		_, data, err := node.Conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		broadMsg(data)
+		fmt.Println("[ws] <<<< ", data)
+	}
+}
+
+var udpsendChan chan []byte = make(chan []byte, 1024)
+
+func broadMsg(data []byte) {
+	udpsendChan <- data
+}
+
+func init() {
+	go udpSendProc()
+	go udpRecvProc()
+}
+
+// 完成udp数据发送协程
+func udpSendProc() {
+	con, err := net.DialUDP("udp", nil, &net.UDPAddr{
+		IP:   net.IPv4(192, 168, 1, 24),
+		Port: 3000,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer con.Close()
+
+	for {
+		select {
+		case data := <-udpsendChan:
+			_, err := con.Write(data)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
+
+// 完成udp数据接收协程
+func udpRecvProc() {
+	con, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4zero,
+		Port: 3000,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer con.Close()
+	for {
+		var buf [512]byte
+		n, err := con.Read(buf[0:])
+		if err != nil {
+			fmt.Println("read buffer error: ", err)
+			return
+		}
+		dispatch(buf[0:n])
+	}
+}
+
+// 后端调度的逻辑处理
+func dispatch(data []byte) {
+	msg := Message{}
+	err := json.Unmarshal(data, &msg)
+	if err != nil {
+		fmt.Println("Unmarshal error: ", err)
+		return
+	}
+	switch msg.Type {
+	case 1: //私信
+		sendMsg(msg.TargetId, data)
+		// case 2: //群发
+		// 	sendGroupMsg()
+		// case 3: //广播
+		// 	sendAllMsg()
+		// case 4:
+
+	}
+}
+
+func sendMsg(userId int64, msg []byte) {
+	rwLocker.RLock()
+	node, ok := clientMap[userId]
+	rwLocker.RUnlock()
+	if ok {
+		node.DataQueue <- msg
+	}
+}
